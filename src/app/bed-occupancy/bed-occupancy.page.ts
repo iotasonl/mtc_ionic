9eeb533930dffa2070594f96d180099aa51348fc
iotasonl <55233409+iotasonl@users.noreply.github.com>
2020-01-05 18:34:12 +0530

    2

---
+++ src/app/bed-occupancy/bed-occupancy.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bed-occupancy',
+  templateUrl: './bed-occupancy.page.html',
+  styleUrls: ['./bed-occupancy.page.scss'],
+})
+export class BedOccupancyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/splash/splash.page.ts
@@ -0,0 +1,23 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage implements OnInit {
+
+  constructor(private router: Router) { this.loadSplashScreen(); 
+  }
+
+  ngOnInit() {
+  }
+
+  loadSplashScreen() {
+    setTimeout(() => {
+      this.router.navigateByUrl('login');
+    }, 3000);
+  }
+
+}
